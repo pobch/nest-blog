24cd7a4985fd71e2af57b06906dd38df253d139f
Pob <590650@gmail.com>
2021-10-09 14:43:32 +0700

    Add hashing password on signup

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,37 @@
 import { Prisma, User } from '@prisma/client'
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, HttpException, HttpStatus, Injectable } from '@nestjs/common'
 import { PrismaService } from 'src/prisma.service'
 import { CreateUserDto } from './dto/create-user.dto'
 import { UpdateUserDto } from './dto/update-user.dto'
+import * as argon2 from 'argon2'
 
 @Injectable()
 export class UserService {
   constructor(private prisma: PrismaService) {}
 
-  async create(createUserDto: CreateUserDto): Promise<User> {
-    return this.prisma.user.create({ data: createUserDto })
+  async create(createUserDto: CreateUserDto): Promise<{ email: string }> {
+    const userNotUnique = await this.prisma.user.findUnique({
+      where: {
+        email: createUserDto.email,
+      },
+    })
+    if (userNotUnique) {
+      throw new BadRequestException('E-mail is already exist')
+    }
+
+    const hashedPassword = await argon2.hash(createUserDto.password)
+
+    const data = {
+      email: createUserDto.email,
+      password: hashedPassword,
+    }
+
+    return this.prisma.user.create({
+      data: data,
+      select: {
+        email: true,
+      },
+    })
   }
 
   findAll() {
